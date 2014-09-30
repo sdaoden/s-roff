1722c2305bba7272ed69fd55a83439b0d189888f
Steffen Nurpmeso <sdaoden@users.sf.net>
2014-09-30 16:28:39 +0200

    Adapt src/ute-lookbib (src/utils/lookbib)

--- src/ute-lookbib/lookbib.cpp
+++ src/ute-lookbib/lookbib.cpp
@@ -1,48 +1,44 @@
-// -*- C++ -*-
-/* Copyright (C) 1989-1992, 2000, 2001, 2002, 2003
-   Free Software Foundation, Inc.
-     Written by James Clark (jjc@jclark.com)
+/*@
+ * Copyright (c) 2014 Steffen (Daode) Nurpmeso <sdaoden@users.sf.net>.
+ *
+ * Copyright (C) 1989 - 1992, 2000 - 2003 Free Software Foundation, Inc.
+ *      Written by James Clark (jjc@jclark.com)
+ *
+ * groff is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2, or (at your option) any later
+ * version.
+ *
+ * groff is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with groff; see the file COPYING.  If not, write to the Free Software
+ * Foundation, 51 Franklin St - Fifth Floor, Boston, MA 02110-1301, USA.
+ */
 
-This file is part of groff.
+#include "config.h"
+#include "lookbib-config.h"
 
-groff is free software; you can redistribute it and/or modify it under
-the terms of the GNU General Public License as published by the Free
-Software Foundation; either version 2, or (at your option) any later
-version.
-
-groff is distributed in the hope that it will be useful, but WITHOUT ANY
-WARRANTY; without even the implied warranty of MERCHANTABILITY or
-FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
-for more details.
-
-You should have received a copy of the GNU General Public License along
-with groff; see the file COPYING.  If not, write to the Free Software
-Foundation, 51 Franklin St - Fifth Floor, Boston, MA 02110-1301, USA. */
-
-#include "lib.h"
-
-#include <stdlib.h>
 #include <assert.h>
 #include <errno.h>
+#include <stdlib.h>
 
+#include "cset.h"
 #include "errarg.h"
 #include "error.h"
-#include "cset.h"
-
+#include "lib.h"
+#include "nonposix.h"
+#include "posix.h"
 #include "refid.h"
 #include "search.h"
 
-/* for isatty() */
-#include "posix.h"
-#include "nonposix.h"
-
-extern "C" {
-  const char *Version_string;
-}
-
 static void usage(FILE *stream)
 {
-  fprintf(stream, "usage: %s [-v] [-i XYZ] [-t N] database ...\n",
+  fprintf(stream,
+    "Synopsis: %s [-v] [-i XYZ] [-t N] database ...\n",
 	  program_name);
 }
 
@@ -80,7 +76,7 @@ int main(int argc, char **argv)
       }
     case 'v':
       {
-	printf("GNU lookbib (groff) version %s\n", Version_string);
+	printf(L_LOOKBIB " (" T_ROFF ") v" VERSION);
 	exit(0);
 	break;
       }
@@ -141,3 +137,4 @@ int main(int argc, char **argv)
   return 0;
 }
 
+// s-it2-mode
