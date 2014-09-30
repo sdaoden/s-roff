c3bf048f80ca0f4a0ad16dd9b7724c3f678a672a
Steffen Nurpmeso <sdaoden@users.sf.net>
2014-09-30 16:28:36 +0200

    Adapt src/ute-lkbib (src/utils/lkbib)

--- src/ute-lkbib/lkbib.cpp
+++ src/ute-lkbib/lkbib.cpp
@@ -1,41 +1,42 @@
-// -*- C++ -*-
-/* Copyright (C) 1989-1992, 2000, 2001 Free Software Foundation, Inc.
-     Written by James Clark (jjc@jclark.com)
+/*@
+ @ Copyright (c) 2014 Steffen (Daode) Nurpmeso <sdaoden@users.sf.net>.
+ *
+ * Copyright (C) 1989 - 1992, 2000, 2001 Free Software Foundation, Inc.
+ *      Written by James Clark (jjc@jclark.com)
+ *
+ * groff is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2, or (at your option) any later
+ * version.
+ *
+ * groff is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with groff; see the file COPYING.  If not, write to the Free Software
+ * Foundation, 51 Franklin St - Fifth Floor, Boston, MA 02110-1301, USA.
+ */
 
-This file is part of groff.
+#include "config.h"
+#include "lkbib-config.h"
 
-groff is free software; you can redistribute it and/or modify it under
-the terms of the GNU General Public License as published by the Free
-Software Foundation; either version 2, or (at your option) any later
-version.
-
-groff is distributed in the hope that it will be useful, but WITHOUT ANY
-WARRANTY; without even the implied warranty of MERCHANTABILITY or
-FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
-for more details.
-
-You should have received a copy of the GNU General Public License along
-with groff; see the file COPYING.  If not, write to the Free Software
-Foundation, 51 Franklin St - Fifth Floor, Boston, MA 02110-1301, USA. */
-
-#include "lib.h"
-
-#include <stdlib.h>
-#include <errno.h>
 #include <assert.h>
-
-#include "errarg.h"
-#include "error.h"
+#include <errno.h>
+#include <stdlib.h>
 
 #include "defs.h"
+#include "errarg.h"
+#include "error.h"
+#include "lib.h"
 #include "refid.h"
 #include "search.h"
 
-extern "C" const char *Version_string;
-
 static void usage(FILE *stream)
 {
-  fprintf(stream, "usage: %s [-nv] [-p database] [-i XYZ] [-t N] keys ...\n",
+  fprintf(stream,
+    "Synopsis: %s [-nv] [-p database] [-i XYZ] [-t N] keys ...\n",
 	  program_name);
 }
 
@@ -79,7 +80,7 @@ int main(int argc, char **argv)
       }
     case 'v':
       {
-	printf("GNU lkbib (groff) version %s\n", Version_string);
+	printf(L_LKBIB " (" T_ROFF ") v" VERSION);
 	exit(0);
 	break;
       }
@@ -135,3 +136,5 @@ int main(int argc, char **argv)
   }
   return !count;
 }
+
+// s-it2-mode
