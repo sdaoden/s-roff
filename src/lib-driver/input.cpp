748169d7c74821b903a8d94868f6a417cb05813a
Steffen Nurpmeso <sdaoden@users.sf.net>
2015-05-29 15:22:54 +0200

    Copyright text: change "groff" to "This"

--- src/lib-driver/input.cpp
+++ src/lib-driver/input.cpp
@@ -14,12 +14,12 @@
  *    Written by James Clark (jjc@jclark.com)
  *    Major rewrite 2001 by Bernd Warken (bwarken@mayn.de)
  *
- *    groff is free software; you can redistribute it and/or modify it
+ *    This is free software; you can redistribute it and/or modify it
  *    under the terms of the GNU General Public License as published by
  *    the Free Software Foundation; either version 2, or (at your option)
  *    any later version.
  *
- *    groff is distributed in the hope that it will be useful, but
+ *    This is distributed in the hope that it will be useful, but
  *    WITHOUT ANY WARRANTY; without even the implied warranty of
  *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *    General Public License for more details.
--- src/troff/mtsm.h
+++ src/troff/mtsm.h
@@ -6,12 +6,12 @@
  * Copyright (C) 2003, 2004 Free Software Foundation, Inc.
  *    written by Gaius Mulley (gaius@glam.ac.uk)
  *
- * groff is free software; you can redistribute it and/or modify it under
+ * This is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free
  * Software Foundation; either version 2, or (at your option) any later
  * version.
  *
- * groff is distributed in the hope that it will be useful, but WITHOUT ANY
+ * This is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
  * for more details.
